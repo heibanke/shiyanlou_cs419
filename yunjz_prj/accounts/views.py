b04467f146e4b8aa3e48c233db63543b1a1232e8
heibanke <heibanke@aliyun.com>
2015-09-01 23:29:19 +0800

    shiyan3 not complete

---
+++ yunjz_prj/accounts/views.py
@@ -0,0 +1,78 @@
+#coding=utf-8
+#django package
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.contrib.auth.models import User  
+from django.template.context import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login as auth_login ,logout as auth_logout
+
+#myApp package
+from accounts.forms import  RegisterForm
+
+def index(request):
+    username = "heibanke"
+    return render(request,"accounts/index.html")
+
+
+def register(request):
+    '''注册视图'''
+    template_var={}
+    form = RegisterForm()    
+    if request.method=="POST":
+        form=RegisterForm(request.POST.copy())
+        if form.is_valid():
+            username=form.cleaned_data["username"]
+            email=form.cleaned_data["email"]
+            password=form.cleaned_data["password"]
+            user=User.objects.create_user(username,email,password)
+            user.save()
+
+            if _login(request,username,password,template_var):
+                return HttpResponseRedirect("/accounts/index")
+        else:
+            aa= form.errors
+            print form.non_field_errors
+            print aa.viewitems()
+    template_var["form"]=form 
+             
+    return render(request,"accounts/register.html",template_var)
+
+	
+def login(request):
+    '''登陆视图'''
+    template_var={}
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if _login(request,username,password,template_var):
+            try:
+                tmp = request.GET['next']
+                return HttpResponseRedirect(tmp)
+            except:
+                return HttpResponseRedirect("/accounts/index")
+ 
+        template_var.update({"username":username}) 
+    return render(request,"accounts/login.html",template_var)
+
+
+def _login(request,username,password,dict_var):
+    '''登陆核心方法'''
+    ret=False
+    user=authenticate(username=username,password=password)
+    if user is not None:
+        if user.is_active:
+            auth_login(request,user)
+            ret=True
+        else:
+            dict_var["error"] = u'用户'+username+u'没有激活'
+    else:
+        dict_var["error"] = u'用户'+username+u'不存在'  
+    return ret
+
+
+def logout(request):
+    auth_logout(request)
+    return render(request,'accounts/logout.html',)	
+	
